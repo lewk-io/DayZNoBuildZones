4ad348aef27ae9daf4805c13a1f4a65faad3783a
lewk-io <76540899+lewk-io@users.noreply.github.com>
2021-02-17 14:03:37 +0000

    Fix IgnoreItems using display names when it should use class/type names

--- Scripts/4_World/Actions/Continuous/ActionDeployObject.c
+++ Scripts/4_World/Actions/Continuous/ActionDeployObject.c
@@ -2,7 +2,7 @@ modded class ActionDeployObject
 {			 	
 	override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
 	{
-		if ( player.GetIsInNoBZ() && ( NoBZ().IgnoredItems.Find( item.GetDisplayName() ) < 0 ) )
+		if ( player.GetIsInNoBZ() && ( NoBZ().IgnoredItems.Find( item.GetType() ) < 0 ) )
         {
             NoBZ().SendWarningMessage( player );
             return false;
